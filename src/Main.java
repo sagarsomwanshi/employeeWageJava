cdc9e77772415bae2150087f2d3a12d159820cce
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-01-27 18:24:30 +0530

    Checks attendance of employee randomly

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int attendance = (int)((Math.random()*10)%2);
+    if(attendance==0) {
+    System.out.println("Employee is Present");
     }
-}
+    else {
+        System.out.println("Employee is Absent");
+        }
+      }
+    }
