79a9857ce01263b9fee6f98869f8afcc5d608b8b
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-01-27 18:49:04 +0530

    Part Time Employee Added

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,25 @@ public class Main {
         int wagePerHour = 20;
 
         int attendance = (int)((Math.random()*10)%2);
+        int partAttendance = (int)((Math.random()*10)%2);
     if(attendance==0) {
-    System.out.println("Employee is Present");
+    System.out.println("Regular Employee is Present");
     int wage = 20*8;
-        System.out.println("Daily Wage of Employee : " +wage);
+        System.out.println("Daily Wage of Regular Employee : " +wage);
     }
-    else {
-        System.out.println("Employee is Absent");
-        System.out.println("Daily wage of Employee : 0");
+     else {
+        System.out.println("Regular Employee is Absent");
+        System.out.println("Daily wage of Regular Employee : 0");
+        }
+
+    if(partAttendance==0) {
+            System.out.println("Part Time Employee is Present");
+            int partWage = 20*8;
+            System.out.println("Daily Wage of Part Time Employee : " +partWage);
+    }
+     else {
+            System.out.println("Part Time Employee is Absent");
+            System.out.println("Daily wage of Part Time Employee : 0");
         }
       }
     }
