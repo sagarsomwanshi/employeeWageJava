c5a7292f31196bfb30c9cf0b054e217797d9f1d3
sagarsomwanshi <sagarsomwanshi13@gmail.com>
2023-01-27 18:35:27 +0530

    Calculates Daily Wage of Employee

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        int wagePerHour = 20;
 
         int attendance = (int)((Math.random()*10)%2);
     if(attendance==0) {
     System.out.println("Employee is Present");
+    int wage = 20*8;
+        System.out.println("Daily Wage of Employee : " +wage);
     }
     else {
         System.out.println("Employee is Absent");
+        System.out.println("Daily wage of Employee : 0");
         }
       }
     }
